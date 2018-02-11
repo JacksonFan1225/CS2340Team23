744151765b9e3420842ca49298702c2b580915ff
Harry Wang <harrywang703@gmail.com>
2018-02-11 17:46:45 -0500

    modified sign in button to only say "sign in"

---
+++ Documents/_CS2340/CS2340Team23Dijkstra/DijkstraPathfinder/app/src/main/java/com/example/harrywang/dijkstrapathfinder/WelcomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.harrywang.dijkstrapathfinder;
+
+import android.os.Build;
+import android.os.Bundle;
+
+/**
+ * The welcome screen that has both a login and register button leading to the respective activities
+ */
+
+public class WelcomeScreen {
+    protected void onCreate(Bundle savedInstanceState ) {
+
+    }
+}
