cccf54df73000bef3301c61187ce64a050e94893
Harry Wang <harrywang703@gmail.com>
2018-02-13 12:47:41 -0500

    Modified layout of welcome screen to be cleaner on backend, added logic to welcome screen

--- Documents/_CS2340/CS2340Team23Dijkstra/DijkstraPathfinder/app/src/main/java/com/example/harrywang/dijkstrapathfinder/WelcomeScreen.java
+++ Documents/_CS2340/CS2340Team23Dijkstra/DijkstraPathfinder/app/src/main/java/com/example/harrywang/dijkstrapathfinder/WelcomeScreen.java
@@ -1,14 +1,28 @@
 package com.example.harrywang.dijkstrapathfinder;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * The welcome screen that has both a login and register button leading to the respective activities
  */
 
-public class WelcomeScreen {
-    protected void onCreate(Bundle savedInstanceState ) {
+public class WelcomeScreen extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+    /**
+     * Sends user to login screen if login button is tapped.
+     */
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginScreen.class);
+        startActivity(intent);
     }
 }
